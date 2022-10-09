73cae501e086bab84426b5b1378aa089851f7118
Ben Daniels <benjamindaniels21@gmail.com>
2022-10-08 20:43:19 -0400

    styling header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import '../App.css'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header-container'>
+      <ul className='navbar'>
+        <li>Home</li>
+        <li>Products</li>
+        <li>Contact</li>
+      </ul>
+
+    </div>
   )
 }
 
